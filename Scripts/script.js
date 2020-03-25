00de6764fea77b7926b671dc933920c3f0699c92
Samuel Jansem <samuel.jansem@gmail.com>
2020-03-25 16:21:49 -0300

    git ignore add

--- Scripts/script.js
+++ Scripts/script.js
@@ -3,5 +3,9 @@ $(() => {
 })
 
 async function initialize() {
-  const { getCurrentCulture, getDateFormatByCulture } = await import('./utils.js')
+  const { getCurrentCulture, getDateFormatByCulture, getDateKeyByDate } = await import('./utils.js')
+  let date = '03/29/2020'
+  let type = 'm'
+  let dk = getDateKeyByDate(date, type)
+  console.log(dk)
 }
--- Scripts/utils.js
+++ Scripts/utils.js
@@ -6,3 +6,34 @@ export const getDateFormatByCulture = () => {
   if (window.navigator.language == 'en-US') return 'MM/DD/YYYY'
   if (window.navigator.language == 'pt-BR') return 'DD/MM/YYYY'
 }
+
+export const getDateKeyByDate = (date, type) => {
+  let culture = getCurrentCulture()
+  let day
+  let week
+  let month
+  let year
+
+  if (culture == 'en-US') {
+    day = date.substr(3, 2)
+    week = moment(date).isoWeek()
+    month = date.substr(0, 2)
+    year = date.substr(6, 4)
+  }
+  if (culture == 'pt-BR') {
+    day = date.substr(0, 2)
+    week = moment(date).isoWeek()
+    month = date.substr(3, 2)
+    year = date.substr(6, 4)
+  }
+
+  if (type == 'd') {
+    return parseInt(`${year}${month}${day}`)
+  }
+  if (type == 'w') {
+    return parseInt(`${year}${week}`)
+  }
+  if (type == 'm') {
+    return parseInt(`${year}${month}`)
+  }
+}
